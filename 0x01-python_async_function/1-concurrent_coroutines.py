51ca0da1f72830040a9424719252e5f57e3bfc59
Magnus984 <nharnharyhaw984@gmail>
2024-10-15 19:48:09 +0000

    Correcting 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,17 +11,16 @@ async def wait_n(n: int, max_delay: int) -> list:
     Gets delay for tasks processing asynchronously
     and returns a sorted list of the delays
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    sorted_delays = []
-    for delay in delays:
-        if len(sorted_delays) == 0:
-            sorted_delays.append(delay)
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        if len(delays) == 0:
+            delays.append(delay)
         else:
-            for i in range(len(sorted_delays)):
-                if delay < sorted_delays[i]:
-                    sorted_delays.insert(i, delay)
+            for i in range(len(delays)):
+                if delay < delays[i]:
+                    delays.insert(i, delay)
                     break
                 else:
-                    sorted_delays.append(delay)
-    return sorted_delays
+                    delays.append(delay)
+    return delays
