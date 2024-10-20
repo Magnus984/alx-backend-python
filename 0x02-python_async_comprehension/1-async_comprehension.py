99fe3b18957f18d257727138c811a6f8977d1ee2
Magnus984 <nharnharyhaw984@gmail>
2024-10-20 21:02:42 +0000

    Fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
-from typing import Callable
+from typing import Callable, List
+
 
 async_generator: Callable = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """
     Import async_generator from the previous task
     and then write a coroutine called async_comprehension
